74862bac659d8a60aee700627402892a85dde126
yeahlolnice <mattwilly98@hotmail.com>
2021-01-26 14:14:52 +1000

    Navbar layout complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-// import Navbar from "./components/navbar"
+import Navbar from "./components/navbar"
 import {BrowserRouter, Switch, Route, Link} from "react-router-dom"
 // npm install react-router-dom 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <nav>
-          
-          <Link to="/">Home</Link>
-        </nav>
-      </header>
-      <Switch>
-        <Route exact path="/" render={() => <h1>test</h1>}></Route>
-      </Switch>
+      <Navbar />
     </BrowserRouter>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,35 @@
+import {Switch, Link, Route} from 'react-router-dom'
 
-// import HomePage from "../pages/HomePage"
 function Navbar() {
   return (
     <>
-        <BrowserRouter>
-            <Link to="/" >Home</Link>
-            
-            <Switch>
-                <Route exact path="/" render={() => <h1>test</h1>}/>
-            </Switch>
-        </BrowserRouter>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav" style={{"justifyContent": "center"}}>
+          <ul class="navbar-nav" style={{"width": "100%", "justifyContent": "space-around"}}>
+            <li class="nav-item active">
+              <Link to="/" className="nav-link" >Home</Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/gallery" className="nav-link" >Gallery</Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/about" className="nav-link" >About</Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/contact" className="nav-link" >Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Switch>
+        <Route exact path="/gallery" render={() => <h1>gallery</h1>} />
+        <Route exact path="/about" render={() => <h1>about</h1>} />
+        <Route exact path="/contact" render={() => <h1>contact</h1>} />
+        <Route exact path="/" render={() => <h1>home</h1>} />
+      </Switch>
     </>
   );
 };
