df56f62c8fd165b0ec0ba4a9bef8f32122bf44c9
jacob solomon <jacobsolomon6@icloud.com>
2021-01-27 19:01:30 +1000

    added buttons to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,16 @@
 import { Navbar, Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
 
 export default function Footer() {
   return (
-    <Container style={{ "height": "7vh", "min-height": "55px", "margin": "0"}}>
-      <Navbar expand="lg" variant="light" bg="light">
+    <Container
+      style={{ height: "7vh", "min-height": "55px", "min-width": "100vw" }}
+    >
+      <Navbar variant="light" bg="light" style={{ "align-items": "center" }}>
         <Navbar.Brand href="#">inspiration homes</Navbar.Brand>
+        <Button style={{ float: "right" }}>Login</Button>
+        <Button style={{ float: "right" }}>Sign Up</Button>
       </Navbar>
     </Container>
   );
