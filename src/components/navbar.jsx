c8e498298a8ba5b61ed60ae256f571d2dce436e3
Matthew Williams <mattwilly98@hotmail.com>
2021-01-27 15:02:57 +1100

    navbar imports from bootstrap

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,58 +1,20 @@
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
+
 
 function SiteNav() {
   return (
     <>
-      {/* <nav class="navbar navbar-expand-lg navbar-light bg-light" >
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav" style={{"justifyContent": "center"}}>
-          <ul class="navbar-nav" style={{"width": "100%", "justifyContent": "space-around"}}>
-            <li class="nav-item active">
-              <Link to="/" className="nav-link" >Home</Link>
-            </li>
-            <li class="nav-item">
-              <Link to="/gallery" className="nav-link" >Gallery</Link>
-            </li>
-            <li class="nav-item">
-              <Link to="/about" className="nav-link" >About</Link>
-            </li>
-            <li class="nav-item">
-              <Link to="/contact" className="nav-link" >Contact</Link>
-            </li>
-          </ul>
-        </div>
-  </nav> */}
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar bg="light" expand="lg" style={{"height": "7vh", "min-height": "55px"}}>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="mr-auto" style={{"width": "100%", "justifyContent": "space-around"}}>
+            <Link to="/" className="nav-link" >Home</Link>
+            <Link to="/gallery" className="nav-link" >Gallery</Link>
+            <Link to="/about" className="nav-link" >About</Link>
+            <Link to="/contact" className="nav-link" >Contact</Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Navbar>
     </>
