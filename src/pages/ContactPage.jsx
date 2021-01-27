fcb974c66464d3375c5a6a487fbcebc158522b78
jacob solomon <jacobsolomon6@icloud.com>
2021-01-27 13:28:51 +1000

    changed nav to react component

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,22 @@
+import Jumbotron from "react-bootstrap/jumbotron";
+import Button from "react-bootstrap/button";
+
 function ContactPage() {
-    return(
-        <h1>Contact Page</h1>
-    )
+  return (
+    // if user !logged in
+    <Jumbotron style={{"padding":"100px"}} >
+      <h1>Contact Inspiration Homes!</h1>
+      <p>
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information.
+      </p>
+      <p>
+        <Button variant="primary">Email Us</Button>
+      </p>
+    </Jumbotron>
+
+    // if user logged in
+  );
 }
 
-export default ContactPage
+export default ContactPage;
